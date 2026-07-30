f67786fe4f496864df4f0cc77b08556991f1f8cc
Joey Biotti <joeybiotti@Joeys-MacBook-Pro.local>
2026-07-30 07:57:30 -0400

    Refactor fct_movie_economics to improve incremental logic and enhance readability

--- models/marts/facts/fct_movie_economics.sql
+++ models/marts/facts/fct_movie_economics.sql
@@ -1,22 +1,36 @@
-{{ 
+{{
     config(
         materialized='incremental',
         unique_key='movie_id',
         incremental_strategy='merge'
-        ) 
+    )
 }}
 
-select
-    movie_id,
-    budget,
-    gross_revenue,
-    loaded_at,
-    (gross_revenue - budget) as total_profit,
-    case
-        when budget > 0 then (gross_revenue - budget) / budget
-    end as roi
-from {{ ref('int_movies_deduplicated') }}
+with source as (
+    select
+        movie_id,
+        budget,
+        gross_revenue,
+        loaded_at,
+        (gross_revenue - budget) as total_profit,
+        case
+            when budget > 0 then (gross_revenue - budget) / budget
+        end as roi
+    from {{ ref('int_movies_deduplicated') }}
+)
 
 {% if is_incremental() %}
-where loaded_at > (select max(loaded_at) from {{this}})
+
+, max_loaded as (
+    select max(loaded_at) as max_loaded_at from {{ this }}
+)
+
+select source.*
+from source, max_loaded
+where source.loaded_at > max_loaded.max_loaded_at
+
+{% else %}
+
+select * from source
+
 {% endif %}
